5817cea715772a6c6b4cfca9bae88dbe1eea6f4c
Prashant Parshuramkar <110542623+PrashantPKP@users.noreply.github.com>
2025-11-13 23:14:21 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Resume-builder/', 
+  base: '/Resume-builder/', // ✅ Must match folder name exactly
   plugins: [react()],
 })
